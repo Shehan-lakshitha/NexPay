591689f19d3b9aa9f24ee00d9d96e70e86b9a105
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-03-01 12:52:52 +0530

    logo animation update

--- frontend/src/screens/IntroLogoAnimationScreen.jsx
+++ frontend/src/screens/IntroLogoAnimationScreen.jsx
@@ -11,7 +11,7 @@ const IntroLogoAnimationScreen = () => {
           <View>
             <LottieView
               style={styles.logoAnimation}
-              source={require('../assets/LogoAnimation.json')}
+              source={require('../Assets/logo.json')}
               autoPlay
             />
           </View>
